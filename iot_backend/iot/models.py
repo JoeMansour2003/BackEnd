fa9ece30b9f1a9ef74b54d8efb994d30d82a3bee
Joseph Mansour <joemansour2003@gmail.com>
2025-07-17 17:39:58 -0400

    mandatory fields

--- iot_backend/iot/models.py
+++ iot_backend/iot/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Iot_Device(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=700, blank=True, null=True)
+    description = models.CharField(max_length=700)
     IP_Address = models.GenericIPAddressField(protocol="both", unpack_ipv4=True, blank=True, null=True)
     Mac_Address = models.CharField(max_length=17, unique=True, blank=True, null=True)
     sector = models.ManyToManyField('Sector', related_name='Associated_Sector')
@@ -22,27 +22,26 @@ class Threat(models.Model):
         ('Medium', 'Medium'),
         ('High', 'High'),
         ('Critical', 'Critical')
-    ], blank=True, null=True)
-    attack_Name = models.CharField(max_length=50, blank=True, null=True) # DOS
+    ], blank=False)
+    attack_Name = models.CharField(max_length=50) # DOS
     description = models.CharField(max_length=700, blank=True, null=True) 
     devices = models.ManyToManyField('Iot_Device', related_name='threats')
 
     def __str__(self):
-        return self.threat_Level + "-"  + self.attack_Name
+        return  self.attack_Name  + ": "  + self.threat_Level
     
 class Threat_Detail(models.Model):
-    Threat = models.ForeignKey('Threat', blank=True, null=True, on_delete=models.CASCADE, related_name='threat_details') 
-    threat_Info_Category = models.ForeignKey('Threat_Info_Category', on_delete=models.CASCADE,blank=True, null=True) #known threats actors
+    Threat = models.ForeignKey('Threat', on_delete=models.CASCADE, related_name='threat_details') 
+    threat_Info_Category = models.ForeignKey('Threat_Info_Category', on_delete=models.CASCADE) #known threats actors
     ai_summary = models.CharField(max_length=125,blank=True, null=True)
-    details = models.TextField(blank=True, null=True) # China
-
-        
+    details = models.TextField() # China
+    
     def __str__(self):
         return str(self.Threat) +": "+str(self.threat_Info_Category) #f"{self.threat.attack_Name} - {self.topic_of_details.topic}"
 
 class Threat_Info_Category(models.Model):
-    topic = models.CharField(max_length=50, unique=True,blank=True, null=True) #known threats actors
-    description = models.CharField(max_length=700, blank=True, null=True)
+    topic = models.CharField(max_length=50, unique=True) #known threats actors
+    description = models.CharField(max_length=350, null=True , blank=True) # a list of known threat actors
 
     def __str__(self):
         return self.topic
